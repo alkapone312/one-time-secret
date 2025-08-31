0dc6eb174c0e7630e533f2a1b5cde50bdf7eb0e0
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2025-08-31 21:04:57 +0200

    Minor corrections

--- script.js
+++ script.js
@@ -36,6 +36,10 @@ function fillHref(selector, value) {
     document.querySelectorAll(selector).forEach(el => el.href = value);
 }
 
+function fillSrc(selector, value) {
+    document.querySelectorAll(selector).forEach(el => el.src = value);
+}
+
 function resetActionButton(label, handler) {
     let actionButton = document.querySelector('.action');
     const newButton = actionButton.cloneNode(true);
@@ -155,7 +159,9 @@ async function loadMetadata() {
     try {
         const data = await (await fetch("metadata.json")).json();
         fillText('.domain-name', data.domainName);
+        fillHref('.main-page', data.mainPage);
         fillHref('.source-link', data.sourceLink);
+        fillSrc('.logo', data.logoSrc);
     } catch {
         console.warn("metadata.json could not be loaded.");
     }
